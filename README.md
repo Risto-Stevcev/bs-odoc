2a3dc0cf4652f6f4d37b7683bea0c8fb2fd50415
Risto Stevcev <risto1@gmail.com>
2018-10-29 02:13:34 +0100

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # bs-odoc
- Helper package to run odoc for bucklescript projects 
+
+Helper package to run [odoc][1] for bucklescript projects 
+
+## Install
+
+Install the package:
+
+```sh
+npm install --save-dev @ristostevcev/bs-odoc
+```
+
+Optionally add the executable to your `scripts` in `package.json`:
+
+```json
+{
+  ...
+  "scripts": {
+    "clean": "bsb -clean-world",
+    "build": "bsb -make-world",
+    "watch": "bsb -make-world -w",
+    "docs": "bs-odoc"
+  },
+  ...
+}
+```
+
+It should work without configuring anything, but you need to have `odoc` 
+installed. See the [README][1] for how to install odoc for bucklescript.
+
+If you have odoc installed, it might not be on your `PATH`. Make sure you ran 
+the opam command to fetch it's path info:
+
+```sh
+eval `opam config env`
+```
+
+## License
+
+See LICENSE
+
+[1]: https://github.com/ocaml/odoc
---
+++ odoc.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+readonly PKG_NAME_RE='.*\"name\":\s*"\(.*\)".*'
+readonly PKG_NAME=$(
+  cat bsconfig.json | grep "\"name\":" | sed -e 's/'"${PKG_NAME_RE}"'/\1/g'
+)
+readonly DOCS=${1:-docs}
+
+readonly ODOC=$(which odoc)
+readonly LIB=./lib/bs
+
+readonly CMT_FILES=$(find ${LIB} -name "*.cmt")
+readonly ODOC_FILES=$(echo ${CMT_FILES} | sed "s/cmt/odoc/g")
+readonly INCLUDES=$(find ${LIB} -type d | tail -n +2 | xargs echo | sed -e 's/ / -I /g' -e 's/^/-I /')
+
+function cleanup_folder {
+  echo "<< Cleanup folder..."
+  rm -rf ${DOCS}
+  mkdir ${DOCS}
+  echo ">> Done!"
+}
+
+function compile_docs {
+  echo "<< Compiling docs..."
+  for file in ${CMT_FILES}; do
+    ${ODOC} compile \
+      ${INCLUDES} \
+      --pkg=${PKG_NAME} \
+      ${file}
+  done
+  echo ">> Done!"
+}
+
+function generate_html {
+  echo "<< Generating HTML..."
+  for file in ${ODOC_FILES}; do
+    ${ODOC} html \
+      ${INCLUDES} \
+      -o ${DOCS} \
+      --semantic-uris \
+      ${file}
+  done
+  echo ">> Done!"
+}
+
+function cleanup_strings {
+  echo "<< Cleanup strings..."
+  find ${DOCS} -type f -name "*.html" -exec \
+    perl -i.bak -pe 's|\&quot;BS:.*?@\&quot;|\&quot;BS\&quot;|g' {} +
+  echo ">> Done!"
+}
+
+function add_support_files {
+  echo "<< Add support files..."
+  ${ODOC} support-files -o ${DOCS}
+  find ${DOCS} -name "*.css" -exec \
+    sed -i -e 's/max-width: 90ex;//' -e 's/margin-left: calc(10vw + 20ex);/margin-left: 20px;/' {} +
+  echo ">> Done!"
+}
+
+if which odoc; then
+  cleanup_folder
+  compile_docs
+  generate_html
+  cleanup_strings
+  add_support_files
+  echo "Finished!"
+else
+  echo -e "\nCouldn't find odoc. Make sure it's on your \$PATH"
+  exit 1
+fi
