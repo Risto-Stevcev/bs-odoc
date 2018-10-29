02563de904359ba6dd675aa8f18dfdd846ccf6cb
Risto Stevcev <risto1@gmail.com>
2018-10-29 11:10:04 +0100

    Match the entire file for cleanup_strings and some code cleanup

--- odoc.sh
+++ odoc.sh
@@ -19,24 +19,29 @@ readonly INCLUDES=$(find ${LIB} -type d | tail -n +2 | xargs echo | sed -e 's/ /
 
 function cleanup_folder {
   echo "<< Cleanup folder..."
+
   rm -rf ${DOCS}
   mkdir ${DOCS}
+
   echo ">> Done!"
 }
 
 function compile_docs {
   echo "<< Compiling docs..."
+
   for file in ${CMT_FILES}; do
     ${ODOC} compile \
       ${INCLUDES} \
       --pkg=${PKG_NAME} \
       ${file}
   done
+
   echo ">> Done!"
 }
 
 function generate_html {
   echo "<< Generating HTML..."
+
   for file in ${ODOC_FILES}; do
     ${ODOC} html \
       ${INCLUDES} \
@@ -44,31 +49,43 @@ function generate_html {
       --semantic-uris \
       ${file}
   done
+
   echo ">> Done!"
 }
 
 function cleanup_strings {
   echo "<< Cleanup strings..."
+
+  # Remove the strange characters coming from external declarations using the bucklescript ppx
   find ${DOCS} -type f -name "*.html" -exec \
-    perl -i.bak -pe 's|\&quot;BS:.*?@\&quot;|\&quot;BS\&quot;|g' {} +
+    perl -i.bak -0777 -pe 's|\&quot;BS:.*?@\&quot;|\&quot;BS\&quot;|gs' {} +
+
   echo ">> Done!"
 }
 
 function add_support_files {
   echo "<< Add support files..."
+
   ${ODOC} support-files -o ${DOCS}
+
+  # Remove the width restriction and set the left margin to be the same as the right
   find ${DOCS} -name "*.css" -exec \
     sed -i -e 's/max-width: 90ex;//' -e 's/margin-left: calc(10vw + 20ex);/margin-left: 20px;/' {} +
+
   echo ">> Done!"
 }
 
-if which odoc; then
+function run_odoc {
   cleanup_folder
   compile_docs
   generate_html
   cleanup_strings
   add_support_files
   echo "Finished!"
+}
+
+if which odoc; then
+  run_odoc
 else
   echo -e "\nCouldn't find odoc. Make sure it's on your \$PATH"
   exit 1
